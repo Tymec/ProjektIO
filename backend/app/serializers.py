33205c6db8cbdd9471e2f04f74baca953afaf49e
Tymec <tymek1rt@hotmail.com>
2023-06-04 01:25:30 +0200

    feat(backend): add api endpoints

---
+++ backend/app/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Order, OrderItem, Product, Review, ShippingAddress
+
+
+# Create your serializers here.
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class ReviewSerializer(ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class OrderSerializer(ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+
+class OrderItemSerializer(ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = "__all__"
+
+
+class ShippingAddressSerializer(ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = "__all__"
