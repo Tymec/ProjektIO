aa3128502247bc47b10d071f624c9be42efffc45
Konrad Sitek <konradsitek270@gmail.com>
2023-06-08 22:09:18 +0200

    color changed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
 
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>PromptWorld</Navbar.Brand>
