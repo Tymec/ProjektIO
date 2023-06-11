2a7c0b201f2d7b3e518a1e8fd9d8ff4f5b45ec8d
Tymec <tymek1rt@hotmail.com>
2023-06-11 20:01:24 +0200

    tests(frontend): add chatbox test

--- frontend/src/components/ChatBox.test.jsx
+++ frontend/src/components/ChatBox.test.jsx
@@ -1,18 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 
-import storeConfig from '../store';
+import { StoreProviderWrapper } from '../testUtils';
 import ChatBox from './ChatBox';
 
 describe('ChatBox Component Tests', () => {
-  test('renders chat box', () => {
-    const store = configureStore(storeConfig);
-
-    render(
-      <Provider store={store}>
+  test('', () => {
+    const { getByText } = render(
+      <StoreProviderWrapper>
         <ChatBox />
-      </Provider>
+      </StoreProviderWrapper>
     );
+
+    const text = getByText('Open chat');
+    expect(text).toBeInTheDocument();
+  });
+
+  test('opens chat', async () => {
+    const { getByText } = render(
+      <StoreProviderWrapper>
+        <ChatBox />
+      </StoreProviderWrapper>
+    );
+
+    const input = getByText('Open chat');
+    fireEvent.click(input);
+
+    await waitFor(() => {
+      expect(input.value).toBe('');
+
+      const sendButton = getByText('Send');
+      const resetButton = getByText('Reset');
+
+      expect(sendButton).toBeInTheDocument();
+      expect(resetButton).toBeInTheDocument();
+    });
   });
 });
