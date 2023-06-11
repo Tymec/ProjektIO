9404fdba83a88a4a990c877dfe173305439f6846
Tymec <tymek1rt@hotmail.com>
2023-06-11 20:01:12 +0200

    build(frontend): update coverage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,15 @@ export default defineConfig({
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'json-summary', 'json'],
-      reportsDirectory: './coverage'
-    }
+      reportsDirectory: './coverage',
+      lines: 20,
+      branches: 20,
+      functions: 20,
+      statements: 20,
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        "src/mock/**", "src/features/**", "src/routes/**"
+      ]
+    },
   }
 });
